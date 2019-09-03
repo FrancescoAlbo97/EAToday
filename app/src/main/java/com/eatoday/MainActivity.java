24dcd030dff0025f821a15d6e7e57a494ca46dec
FrancescoAlbo97 <fralbo97@gmail.com>
2019-09-04 01:58:05 +0200

    recipe adapter without image

--- app/src/main/java/com/eatoday/MainActivity.java
+++ app/src/main/java/com/eatoday/MainActivity.java
@@ -6,18 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.eatoday.model.Recipe;
+import com.eatoday.ui.recipes.RecipeAdapter;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecipeAdapter.ItemClicked{
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    RecyclerView recyclerViewRecipe;
+    RecyclerView.Adapter myRecipeAdapter;
+    RecyclerView.LayoutManager layoutManagerRecipe;
+    ArrayList<Recipe> recipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,36 @@ public class MainActivity extends AppCompatActivity {
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        recyclerViewRecipe = findViewById(R.id.list);
+        recyclerViewRecipe.setHasFixedSize(true);
+        layoutManagerRecipe = new LinearLayoutManager(this);
+        recyclerViewRecipe.setLayoutManager(layoutManagerRecipe);
+        String pizza = "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjM0bnjrLLkAhUyAGMBHbJPC18QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.bofrost.it%2Fprodotti%2Fpizze-snack%2Fpizze%2Fpizza-la-margherita%2F&psig=AOvVaw2kcnthPL-PE7gAjRnTiWpm&ust=1567520959713443.";
+        String aperitivo = "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjlop-IrbLkAhVJ8uAKHSDfDGkQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.gazzettadiparma.it%2Farchivio%2F2012%2F07%2F08%2Fnews%2Faperitivo_e_buffet_quanto_costa_la_movida_-716782%2F&psig=AOvVaw0lvOoHmdtSelDix7q-9LCV&ust=1567521039453848";
+        recipes = new ArrayList<>();
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("aperitivo", "30", "facile","economico","vegetariano","ciaociao",aperitivo));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("aperitivo", "30", "facile","economico","vegetariano","ciaociao",aperitivo));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("aperitivo", "30", "facile","economico","vegetariano","ciaociao",aperitivo));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("aperitivo", "30", "facile","economico","vegetariano","ciaociao",aperitivo));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("aperitivo", "30", "facile","economico","vegetariano","ciaociao",aperitivo));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+        recipes.add(new Recipe("aperitivo", "30", "facile","economico","vegetariano","ciaociao",aperitivo));
+        recipes.add(new Recipe("pizza", "30", "facile","economico","vegetariano","ciaociao",pizza));
+
+        myRecipeAdapter = new RecipeAdapter((Context) MainActivity.this, recipes);
+        recyclerViewRecipe.setAdapter(myRecipeAdapter);
 
         drawerLayout = this.findViewById(R.id.drawer_layout);
         navigationView = this.findViewById(R.id.navigationView);
@@ -81,5 +122,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClicked(int index) {
+
     }
 }
---
+++ app/src/main/java/com/eatoday/ui/recipes/RecipeAdapter.java
@@ -0,0 +1,73 @@
+package com.eatoday.ui.recipes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.eatoday.R;
+import com.eatoday.model.Recipe;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+
+    private ArrayList<Recipe> recipes;
+    ItemClicked activity;
+    Context context;
+
+    public RecipeAdapter(Context context, ArrayList<Recipe> list){
+        this.recipes = list;
+        this.activity = (ItemClicked) context;
+        this.context = context;
+    }
+
+    public interface ItemClicked{
+        void onItemClicked(int index);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView ivImageRecipe;
+        TextView tvRecipeName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivImageRecipe = itemView.findViewById(R.id.ivImageRecipe);
+            tvRecipeName = itemView.findViewById(R.id.tvRecipeName);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    activity.onItemClicked(recipes.indexOf((Recipe) view.getTag()));
+                }
+            });
+        }
+    }
+    @NonNull
+    @Override
+    public RecipeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeAdapter.ViewHolder holder, int position) {
+        holder.itemView.setTag(recipes.get(position));
+        holder.tvRecipeName.setText(recipes.get(position).getName());
+        //holder.ivImageRecipe.setImageResource(rec);
+        Picasso.with(context).load(recipes.get(position).getImageUrl()).fit().centerInside().into(holder.ivImageRecipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+}
